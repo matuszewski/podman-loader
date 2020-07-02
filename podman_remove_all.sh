9b700cf59de2f6fc2da343350cef22d858929b45
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2020-07-02 21:32:22 +0200

    Update podman_remove_all.sh

--- podman_remove_all.sh
+++ podman_remove_all.sh
@@ -1,8 +1,8 @@
-#!/bin/sha
+#!/bin/sh
 # fully removes podman images, pods and containers
 podman kill -a
 podman rm -a
 podman pod rm -a
 podman rm -a
 podman rmi -a
-echo '[>] DONE --------------------------'
+echo '[>] done'
