7d4fa0deca92376cb398fb844a0ecf7ceb87e63e
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2020-07-02 21:30:12 +0200

    Added file counters

--- podman_loader.py
+++ podman_loader.py
@@ -1,5 +1,5 @@
-# #!/usr/bin/env python2.7
-#       python2.7 -m compileall .    # compilation to .pyc extension
+#!/usr/bin/env python
+# >>> python2.7 -m compileall .    # compilation to .pyc extension
 
 import subprocess
 from os import listdir
@@ -10,20 +10,20 @@ directory = "."             # .     working directory
 extension = ".tar"          # .tar
 
 def main():
-    #command = "podman load --input "   # default command to run
+    # command = "podman load --input "   # default command to run
     command = "echo "
-    
+
 
     # creates a list of all files (also hidden) in specified directory
     allfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
-    
+
     # creates an empty list to which all found *.tar archives will append
     archives = []
 
-    
+
     # Display all files (also hidden)
-    #for filename in onlyfiles:
-    #    print(filename)
+#   for filename in onlyfiles:
+#       print(filename)
 
     print('-'*30)
     print("PODMAN LOADER:")
@@ -31,9 +31,9 @@ def main():
     print("[>] extension:\t" + extension)
     print('-'*30)
 
-    
-    print("all files: " + str(len(allfiles)))
-    
+
+    print("[>] all files: " + str(len(allfiles)))
+
     # display files with *.tar extension
     for filename in allfiles:
         if(filename.find(extension, -4) != -1): # checks if the last 4 characters are the extension ( .XXX )
@@ -41,15 +41,18 @@ def main():
             archives.append(filename)
 
             #subprocess.run(["echo", filename])
-            
+
             #process = subprocess.Popen(command + "ea", stdout=subprocess.PIPE)
             #output, error = process.communicate()
 
+    print("[>] " + extension + " files: " + str(len(archives)))
+
     print("[>] running")
-    
+
     for archive in archives:
         os.system("podman load --input " + archive)
-    
+        print("\t loaded " + archive)
+
     print("[>] done")
 
     pass
